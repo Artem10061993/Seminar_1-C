8e9b98d6b112f0e33991c139053ad941116c42a2
Artem Kandakov <Temofey1993@gmail.com>
2022-08-08 21:16:26 +0500

    Прорешали три задачи

---
+++ Example_1/Program.cs
@@ -0,0 +1,5 @@
+﻿Console.WriteLine("Введите целое число");
+int x = Convert.ToInt32(Console.ReadLine()); 
+int sqr = x * x;
+Console.Write("Квадрат числа = ");
+Console.WriteLine(sqr);
---
+++ Example_2/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Введите первое число");
+int x = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите второе число");
+int y = Convert.ToInt32(Console.ReadLine());
+
+if(x*x==y) 
+{Console.WriteLine("Является");}
+else
+{Console.WriteLine("Не является");}
